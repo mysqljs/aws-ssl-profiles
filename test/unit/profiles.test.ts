2de5243c86b26f7854e1532df237b886a07d8227
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2024-07-14 23:40:53 -0300

    chore: update and upgrade dependencies

--- test/unit/profiles.test.ts
+++ test/unit/profiles.test.ts
@@ -1,12 +1,12 @@
-import { describe, assert } from 'poku';
+import { test, strict } from 'poku';
 import profiles from '../../src/index.js';
 
-describe('Testing Profiles Final Bundle Structure');
-
-assert.strictEqual(typeof profiles, 'object', 'Ensure profiles type');
-assert(Array.isArray(profiles.ca), 'Ensure profiles CA type');
-assert(
-  profiles.ca.every((item) => typeof item === 'string'),
-  'Ensure all profiles CA items are strings'
-);
-assert.strictEqual(profiles.ca.length, 120, 'Ensure profiles CA length');
+test('Testing Profiles Final Bundle Structure', () => {
+  strict.strictEqual(typeof profiles, 'object', 'Ensure profiles type');
+  strict(Array.isArray(profiles.ca), 'Ensure profiles CA type');
+  strict(
+    profiles.ca.every((item) => typeof item === 'string'),
+    'Ensure all profiles CA items are strings'
+  );
+  strict.strictEqual(profiles.ca.length, 120, 'Ensure profiles CA length');
+});
