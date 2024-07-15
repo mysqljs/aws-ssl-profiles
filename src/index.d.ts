05b03610b9cb7b1ab567494149e8d1bfd6f2640d
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2024-07-15 02:03:46 -0300

    chore: standardize typings style

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,8 +1,8 @@
-import type { Profiles } from './@types/profiles.js';
+import type { Profiles } from "./@types/profiles.js";
 export declare const proxyBundle: Profiles;
 declare const profiles: Profiles;
-declare module 'aws-ssl-profiles' {
-  export = profiles;
-  export const proxyBundle: Profiles;
+declare module "aws-ssl-profiles" {
+    export = profiles;
+    export const proxyBundle: Profiles;
 }
 export default profiles;
