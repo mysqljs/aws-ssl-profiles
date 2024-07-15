335147bfaa6829910f728958a754c0aa1957b0ff
Mario Román Dono <mario.romandono@gmail.com>
2024-07-15 03:08:20 +0200

    feat: add RDS Proxy CA certificates (#19)

--- test/unit/verify-certs.test.ts
+++ test/unit/verify-certs.test.ts
@@ -1,6 +1,7 @@
 import x509 from 'x509.js';
 import { describe, assert } from 'poku';
 import { defaults } from '../../src/profiles/ca/defaults.js';
+import { proxy } from '../../src/profiles/ca/proxy.js';
 
 describe('Ensuring all certificates are valid and parsable', {
   pad: true,
@@ -15,5 +16,20 @@ defaults.forEach((cert, index) => {
     parsedCert.issuer.organizationalUnitName,
     expected,
     `Certificate at index ${index} from defaults`
+  );
+});
+
+proxy.forEach((cert, index) => {
+  const parsedCert = x509.parseCert(cert);
+  let expected = 'Amazon';
+
+  if (index === 4) {
+    expected = 'Starfield Technologies, Inc.';
+  }
+
+  assert.strictEqual(
+    parsedCert.issuer.organizationName,
+    expected,
+    `Certificate at index ${index} from proxy`
   );
 });
