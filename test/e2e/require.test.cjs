06ab07fd0bdd01619e9c65a0acefdf4326fdf3ac
HappyZombies <daniel.reguero@hotmail.com>
2024-08-29 13:36:14 -0400

    handle export and update tests

--- test/e2e/require.test.cjs
+++ test/e2e/require.test.cjs
@@ -2,10 +2,12 @@
 
 const { test, strict } = require('poku');
 const awsCaBundle = require('../../lib/index.js');
+const { ca: awsCas } = require('../../lib/index.js').default;
 const { proxyBundle } = require('../../lib/index.js');
 
 test('Testing require (CJS)', () => {
   strict(typeof awsCaBundle?.ca?.length === 'number', 'Default require');
+  strict(typeof awsCas?.length === 'number', 'Default require with default property present');
   strict(typeof proxyBundle?.ca?.length === 'number', '"proxyBundle" require');
   strict(
     typeof awsCaBundle.proxyBundle?.ca?.length === 'number',
